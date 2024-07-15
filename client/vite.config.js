361716378be276a10293db04bd081fa360f3032f
unknown <urmilgadhiya018@gmail.com>
2024-07-15 13:01:05 +0530

    Initial commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,3 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+
+
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true,
+//     port: 5173 
+//   }
+// });
